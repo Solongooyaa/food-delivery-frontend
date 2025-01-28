918fc1428174766394489e8f0c7289475f777c3c
Solongooyaa <u.solongo2018@gmail.com>
2025-01-27 21:24:23 -0800

    frontend

--- src/app/admin/order/page.tsx
+++ src/app/admin/order/page.tsx
@@ -2,6 +2,15 @@
 import { Label } from "@/components/ui/label";
 import { useAuth } from "@clerk/nextjs";
 import { useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function AdminOrderPage() {
   const { getToken } = useAuth();
@@ -22,10 +31,31 @@ export default function AdminOrderPage() {
     <div className="w-[1171px] h-[948px] bg-[#ffffff] rounded">
       <div className="flex justify-between items-center p-6">
         <h1>Orders</h1>
-        <input type="date" className="w-40 h-10 border rounded-xl" />
-        <div>
-          <Label htmlFor="email">Your email address</Label>
-        </div>
+        <input type="date" className="w-40 h-10 border rounded-full" />
+      </div>
+      <div>
+        {data?.map((user: any) => (
+          <div key={user.id}></div>
+        ))}
+        <Table>
+          <TableCaption></TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
